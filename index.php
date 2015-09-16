e65d689f90bc5ada79f4541bd7c33c7a7d5202a5
root <root@training3.training3>
2015-09-16 03:47:31 -0700

    update

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-    // composer
-    require_once( __DIR__.'/vendor/autoload.php' );
+    // composer autoload
+    require_once(__DIR__.'/vendor/autoload.php');
 
     echo 'Hello, your IP is ' . Ydin\Client::getIp();
-    
-
-
