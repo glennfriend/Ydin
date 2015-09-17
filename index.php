1b083d2626d3b0056fb7ed0baafe5b28b015b360
root <root@training3.training3>
2015-09-17 02:48:06 -0700

    update

--- index.php
+++ index.php
@@ -14,43 +14,10 @@
     require_once(__DIR__.'/vendor/autoload.php');
 
     echo '<pre>';
-    if (false) {
+    if (true) {
         echo 'Hello, your IP is ' . Ydin\Client\User::getIp();
         echo '</pre>';
         exit;
     }
-
-$items = [
-        'name'      => 'ken',
-        'age'       => 18,
-        'job'       => 'student',
-        'interest'  => ['football', 'baseball', 'movie'],
-        'report'    => [
-            'math'      => 100,
-            'english'   => 90,
-            'chemical'  => 20,
-        ],
-    ];
-
-    $dot = Ydin\ArrayKit\Dot::factory($items);
-    
-print_R($dot('namexx','aaa')       );
-echo "\n";
-print_R($dot('interest.0')   );  
-echo "\n";
-print_R($dot('interest') );
-echo "\n";
-print_R($dot('report.math'));
-echo "\n";
-
-
-print_R($dot);
-echo "\n";
-
-
-
-$dotInstance = new Ydin\ArrayKit\DotInstance($items);
-echo $dotInstance->get('interest.0');
-
 
 
